f5ebc00861c66e61c38d97c727c2cc26aff00b7d
nicoson <nicoson@163.com>
2015-06-13 10:01:51 -0400

    add strategy

---
+++ S2_MLsystem_core.py
@@ -0,0 +1,71 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 09:52:05 2015
+
+@author: nxh
+"""
+
+
+import MySQLdb
+import random as rd
+import datetime
+
+def startdateGet(tradedate,earningTable):
+    conn=MySQLdb.connect(host="localhost",user="root",passwd="root",db="strategyDatabase")
+    cursor = conn.cursor()
+    sql = "select * from " + earningTable + " order by tradedate desc limit 1"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    if cursor.rowcount <= 0:
+        pass
+    else:
+        sql = "select max(tradedate) from " + earningTable
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        tradedate = max(tradedate, data[0][0] + datetime.timedelta(1))
+    
+    cursor.close()
+    conn.close()
+    return tradedate
+    
+
+def checkCurrent(currentHoldingTable,num):
+    conn=MySQLdb.connect(host="localhost",user="root",passwd="root",db="strategyDatabase")
+    cursor = conn.cursor()
+    sql = "select * from " + currentHoldingTable + " where stockid != '000000'"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    
+    if cursor.rowcount <= 1:   # empty table
+        newStocklist = (num,[])
+    else:
+        #stocklist = [datum[1] for datum in data]
+        stocklist = [];        
+        for datum in data:
+            rate = datum[4]/datum[3]
+            if rate > 1.05 or rate < 0.9:
+                stocklist.append(datum[1])
+                
+        newStocklist = (num - len(data) + len(stocklist), stocklist)
+    
+    cursor.close()
+    conn.close()
+    return newStocklist
+    
+
+def strategy_main(n):
+    conn=MySQLdb.connect(host="localhost",user="root",passwd="root",db="StockDataInfoCenter")
+    cursor = conn.cursor()
+    sql = "select * from " + "StockInfoList"
+    cursor.execute(sql)
+    data = cursor.fetchall();
+    stocklist = [datum[1] for datum in data]
+    conn.close()
+    return strategy_core(stocklist,n)
+
+
+def strategy_core(stocklist,n):
+    loc = rd.sample(xrange(len(stocklist)),n)
+    return [stocklist[i] for i in loc]
+    
--- S2_MLsystem_main.py
+++ S2_MLsystem_main.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 #!usr/bin/python
 """
-Created on Sat May 23 10:41:34 2015
+Created on Sat Jun 13 09:49:34 2015
 
 @author: nxh
 """
 
 import datetime
 import Database_Init as di
-import S1_StochasticStrategy_core as ss
+import S2_MLsystem_core as mls
 
 def mainProcess():
     # 0.configuration
-    holdingHistoryTable = 's1_test_holdingHistory'
-    currentHoldingTable = 's1_test_currentHolding'
-    earningTable = 's1_test_earningTable'
+    holdingHistoryTable = 's2_mls_holdingHistory'
+    currentHoldingTable = 's2_mls_currentHolding'
+    earningTable = 's2_mls_earningTable'
     num = 10    # max number of stocks in this portfolio
     startdate = "2015-01-01"
     amount = 10000  # amount of the new portfolio
@@ -24,9 +24,9 @@ def mainProcess():
     di.database_init(holdingHistoryTable,currentHoldingTable,earningTable,startdate,amount);
     
     # 2.decide the start date to complement the data
-    today = datetime.datetime.today().date()
-    #today = datetime.date(2015,2,20)    
-    tradedate = ss.startdateGet(tradedate,earningTable)
+    #today = datetime.datetime.today().date()
+    today = datetime.date(2015,2,20)    
+    tradedate = mls.startdateGet(tradedate,earningTable)
     
     # 3.run the strategy for each trading day
     while tradedate <= today:
@@ -38,11 +38,11 @@ def mainProcess():
 
 def subProcess(currentHoldingTable, holdingHistoryTable, earningTable, num, tradedate, amount):
     # 3.1 check the currentHolding table to decide whether to make changes
-    items = ss.checkCurrent(currentHoldingTable,num)    #item = (int, list)
+    items = mls.checkCurrent(currentHoldingTable,num)    #item = (int, list)
     if items[0] == 0: # no changes in current portfolio
         newStocklist = []
     else:   # new portfolio
-        newStocklist = ss.strategy_main(items[0])
+        newStocklist = mls.strategy_main(items[0])
     
     # 3.2 make change to the tables
     di.tableArrange(items, newStocklist, currentHoldingTable, holdingHistoryTable, num, tradedate)
